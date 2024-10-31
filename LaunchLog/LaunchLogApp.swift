c8eae5ba2f1a51d269a6793a328c6e8a7efa047c
Jing Li <jingliapply234@yahoo.com>
2024-10-31 13:10:34 +0800

    Initial Commit

---
+++ LaunchLog/LaunchLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchLogApp.swift
+//  LaunchLog
+//
+//  Created by Jing Li on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct LaunchLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LaunchLogTests/LaunchLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchLogTests.swift
+//  LaunchLogTests
+//
+//  Created by Jing Li on 10/31/24.
+//
+
+import Testing
+@testable import LaunchLog
+
+struct LaunchLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
